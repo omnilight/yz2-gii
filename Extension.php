0c5f181293b17a73ed971142ae9c4ded8fbc5f5f
Pavel Agalecky <pavel.agalecky@gmail.com>
2013-12-17 14:07:07 +0400

    Working on Yz

---
+++ Extension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace yz\admin;
+
+
+/**
+ * Class Extension
+ */
+class Extension extends \yii\base\Extension
+{
+	public static function init()
+	{
+		parent::init();
+
+		\Yii::$app->i18n->translations['yz/gii'] = [
+			'class' => 'yii\i18n\PhpMessageSource',
+			'basePath' => '@yz/gii/messages',
+			'sourceLanguage' => 'en-US',
+		];
+	}
+
+} 
