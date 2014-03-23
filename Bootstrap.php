94ed5890ee52a263c922ab1f44722fd6b5f581fe
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-03-23 19:01:11 +0400

    Changed Extension class to Bootstrap

---
+++ Bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace yz\gii;
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+
+
+/**
+ * Class Bootstrap
+ */
+class Bootstrap implements BootstrapInterface
+{
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     * @param Application $app the application currently running
+     */
+    public function bootstrap(Application $app)
+    {
+        $app->i18n->translations['yz/gii'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'basePath' => '@yz/gii/messages',
+            'sourceLanguage' => 'en-US',
+        ];
+    }
+}
