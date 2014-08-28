c91dea1b5254e7e530a26ad3bc903f601e150e91
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-08-28 13:39:10 +0400

    Updated _search template

--- generators/crud/default/views/_search.php
+++ generators/crud/default/views/_search.php
@@ -13,6 +13,7 @@ echo "<?php\n";
 
 use yii\helpers\Html;
 use yz\admin\widgets\ActiveForm;
+use yz\admin\widgets\FormBox;
 
 /**
  * @var yii\web\View $this
@@ -22,8 +23,9 @@ use yz\admin\widgets\ActiveForm;
 ?>
 
 <div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-search hidden" id="filter-search">
-
-    <?= "<?php " ?>$form = ActiveForm::begin([
+    <?= "<?php" ?> $box = FormBox::begin() ?>
+    <?= "<?php" ?> $box->beginBody() ?>
+    <?= "<?php" ?> $form = ActiveForm::begin([
         'action' => ['index'],
         'fieldConfig' => [
             'horizontal' => ['label' => 'col-sm-3', 'input' => 'col-sm-5', 'offset' => 'col-sm-offset-3 col-sm-5'],
@@ -35,18 +37,17 @@ use yz\admin\widgets\ActiveForm;
 $count = 0;
 foreach ($generator->getColumnNames() as $attribute) {
     if (++$count < 6) {
-        echo "    <?= " . $generator->generateActiveSearchField($attribute) . " ?>\n\n";
+        echo "    <?= " . $generator->generateActiveSearchField($attribute) . " ?>\n";
     } else {
-        echo "    <?php // echo " . $generator->generateActiveSearchField($attribute) . " ?>\n\n";
+        echo "    <?php // echo " . $generator->generateActiveSearchField($attribute) . " ?>\n";
     }
 }
 ?>
-        <div class="form-group">
-            <div class="col-sm-offset-3 col-sm-5">
-                <?= "<?= " ?>Html::submitButton(\Yii::t('admin/t','Search'), ['class' => 'btn btn-primary']) ?>
-            </div>
-        </div>
+        <?= "<?php " ?> $box->endBody() ?>
+        <?= "<?php " ?> $box->beginFooter() ?>
+            <?= "<?= " ?>Html::submitButton(\Yii::t('admin/t','Search'), ['class' => 'btn btn-primary']) ?>
+        <?= "<?php " ?> $box->endFooter() ?>
 
     <?= "<?php " ?>ActiveForm::end(); ?>
-
+    <?= "<?php " ?> FormBox::end() ?>
 </div>
